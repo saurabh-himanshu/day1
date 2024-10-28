d110fe798d5d9b3d1b7ab7ec220e5cde380105f7
saurabh-himanshu <saurabhgawasara@gmail.com>
2024-10-28 13:17:25 +0530

    Add files via upload

---
+++ html_dots.js
@@ -0,0 +1,30 @@
+function login()
+{
+    let name=document.getElementById("input").value;
+    let pin=document.getElementById("password").value;
+    console.log(name);
+    console.log(pin);
+    document.getElementById("output").innerText="Login as "+name;
+}
+function createAccount() {
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirm_password').value;
+    if(username==""||email==""||password==""||confirmPassword=="")
+    {
+        alert("All fiels are required!");
+        //document.getElementById('output').textContent='please fill all fields';
+        return;
+    }
+    else if (password !== confirmPassword) {
+        alert("Password and confirm password don't matched");
+        //document.getElementById('output').textContent = 'Passwords do not match!';
+        return;
+    }
+    document.getElementById('create').style.visibility='hidden';
+    document.getElementById('output').textContent = 'Account created successfully!';
+    document.getElementById('lbtn').textContent='login';
+    document.getElementById('lbtn').style.background='blue';
+    document.getElementById('lbtn').style.visibility='visible';
+}
